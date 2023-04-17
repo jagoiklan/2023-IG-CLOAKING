6f0b2007aabb131577d63284a4f1c248c271401e
jagoiklan <84367574+jagoiklan@users.noreply.github.com>
2023-04-17 12:00:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /* Required settings     */
-$CLOAKING['WHITE_PAGE'] = 'a.php';//PHP/HTML file or URL used for bots
+$CLOAKING['WHITE_PAGE'] = 'https://dailysocial.id/post/cara-menambah-followers-di-instagram';//PHP/HTML file or URL used for bots
 $CLOAKING['OFFER_PAGE'] = 'b.php';//PHP/HTML file or URL offer used for real users
 $CLOAKING['DEBUG_MODE'] = 'on';// replace "on" with "off" to switch from debug to production mode
 $CLOAKING['STEALTH'] = 'off';// replace "on" with "off" to hide stat page
